b85c7eee4d9671631031da47f7810f913c1030ec
davidad (David A. Dalrymple) <davidad@alum.mit.edu>
2022-11-21 04:14:16 +0400

    2-player bargaining toy

---
+++ 2/sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'ntu-foo-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './ntu_foo.js',
+  './ntu_foo.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
